05c85db8344594ef1eafb3c5a71625a3efb3dd78
VictorNascimento2000 <73149327+VictorNascimento2000@users.noreply.github.com>
2024-07-14 15:28:28 -0300

    Update LU.py

--- LU.py
+++ LU.py
@@ -1,6 +1,5 @@
 import numpy as np
-A = np.matrix([[0, 4, 1], [1, 1, 3], [2, -2, 1]])
-b = np.array([9, 6, -1])
+A = np.matrix([[2, 10, 8, 8, 6], [1, 4, -2, 4, -1], [0, 2, 3, 2, 1], [3, 8, 3, 10, 9], [1, 4, 1, 2, 1]], dtype=float)
 
 n = A.shape[0]
 intch = np.zeros(n)
@@ -11,23 +10,30 @@ def Gauss(A, n, intch, flag):
     for k in range(0,n):
         amax = float('-inf')
         m = k
-        for i in range(0,n-1):
+        for i in range(k,n):
             val = abs(A[i,k])
             if  val > amax:
                 amax = val
                 m = i
+                
         if amax == 0:
             print(flag)
         else:
             intch[k] = m
             if m != k:
                 A[(m, k), :] = A[(k, m), :]
+            
             for i in range(k+1,n):
                 A[i,k] = A[i,k]/A[k,k]
+
             for i in range(k+1,n):
-                A[i,k:] = A[i,k]*
+                A[i,k+1:] = A[i,k+1:] - A[i,k]*A[k,k+1:] 
+    if A[n-1, n-1] == 0:
+        print(flag)
+        intch[n-1] = 0
+    else:
+         intch[n-1] = n-1
+    print(A)
+    print(intch)
 
-
-        
-        
 Gauss(A, n, intch, flag)
